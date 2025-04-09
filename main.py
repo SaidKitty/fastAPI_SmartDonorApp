417a0e6d1fcdd71046c3e5a9af3c8caa6d3efb16
SaidKitty <33800238+SaidKitty@users.noreply.github.com>
2025-04-09 22:32:32 +0300

    Update main.py
    rearranged features

--- main.py
+++ main.py
@@ -16,37 +16,42 @@ label_encoders = joblib.load('label_encoders.pkl')
 class SmartDonorApp(BaseModel):
     bloodGroup: str
     occupation: str
-    convTime: str
+    yob: int
     timesDonated: str
+    daysSinceLastDonation: int
+    convTime: str
+    prefferedFreq: int
+    rateService: int
     reaction: str
     encouragement: str
     convLocality: str
-    daysSinceLastDonation: int
-    prefferedFreq: int
-    rateService: int
-    yob: int
+   
+   
+   
+   
 
 @app.post("/predict")
 async def predict_score(features: List[SmartDonorApp]):
     input_data = pd.DataFrame([item.dict() for item in features])
 
     # Ensure all expected columns exist
-    expected_columns = ['bloodGroup', 'occupation', 'convTime', 'timesDonated', 
-                        'reaction', 'encouragement', 'convLocality', 'daysSinceLastDonation', 
-                        'prefferedFreq', 'rateService', 'yob']
+    expected_columns = ['bloodGroup', 'occupation', 'yob', 'timesDonated', 
+                        'daysSinceLastDonation', 'convTime',  'prefferedFreq', 'rateService',
+                        'reaction', 'encouragement', 'convLocality' 
+                        ]
     
     missing_cols = [col for col in expected_columns if col not in input_data.columns]
     if missing_cols:
         return {"error": f"Missing columns: {missing_cols}"}
 
     # Encode categorical columns using label encoders
-    categorical_cols = ['bloodGroup', 'occupation', 'convTime', 'reaction', 'timesDonated', 'encouragement', 'convLocality']
+    categorical_cols = ['bloodGroup', 'occupation',  'timesDonated', 'convTime', 'reaction', 'encouragement', 'convLocality']
     for col in categorical_cols:
         le = label_encoders[col]  # Load corresponding encoder
         input_data[col] = le.transform(input_data[col])
 
     # Scale numeric values using the saved scaler
-    numeric_cols = ['daysSinceLastDonation', 'prefferedFreq', 'rateService', 'yob']
+    numeric_cols = ['yob', 'daysSinceLastDonation', 'prefferedFreq', 'rateService' ]
     input_data[numeric_cols] = scaler.transform(input_data[numeric_cols])
 
     # Pass the correctly formatted data to the model
